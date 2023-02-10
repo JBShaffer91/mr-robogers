45b6a2c4a3d88bc0a98db4875d2bad3a1b987dfc
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-10 14:02:58 -0800

    WIP remove the 0 at the start of return

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,9 @@ window.onload = function() {
   const form = document.getElementById('form');
   form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const input = document.getElementById('number').value;
-    const resultArray = getResultArray(input);
+    let inputValue = document.getElementById('number').value;
+    inputValue = inputValue ? parseInt(inputValue) : 0;
+    const resultArray = getResultArray(inputValue);
     const output = document.getElementById('output');
     output.innerHTML = resultArray.join(', ');
   });
