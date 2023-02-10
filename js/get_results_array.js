28e73953f6c34713692d7c583c49c47fbfa3b89a
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-10 13:40:24 -0800

    gather input, loop through array, display limited results

---
+++ js/get_results_array.js
@@ -0,0 +1,19 @@
+// Tests
+//function runTests() {
+//  let resultArray = getResultArray(10);
+//  let expectedArray = [0, 'Beep!', 'Boop!', 'Won\'t you be my neighbor?', 4, 5, 6, 7, 8, 9, 'Beep!'];
+//  let isTestPassing = JSON.stringify(resultArray) === JSON.stringify(expectedArray);
+//  console.log('Test 1', isTestPassing ? 'PASS' : 'FAIL');
+
+//  resultArray = getResultArray(20);
+//  expectedArray = [0, 'Beep!', 'Boop!', 'Won\'t you be my neighbor?', 4, 5, 6, 7, 8, 9, 'Beep!', 11, 'Boop!', 'Won\'t you be my neighbor?', 14, 15, 16, 17, 18, 19, 'Beep!'];
+//  isTestPassing = JSON.stringify(resultArray) === JSON.stringify(expectedArray);
+//  console.log('Test 2', isTestPassing ? 'PASS' : 'FAIL');
+
+//  resultArray = getResultArray(0);
+//  expectedArray = [0];
+//  isTestPassing = JSON.stringify(resultArray) === JSON.stringify(expectedArray);
+//  console.log('Test 3', isTestPassing ? 'PASS' : 'FAIL');
+//}
+
+//runTests();
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business logic
+function getResultArray(input) {
+  const resultArray = [];
+  for (let i = 0; i <= input; i++) {
+    const stringField = i.toString();
+    if (stringField.includes('3')) {
+      resultArray.push('Won\'t you be my neighbor?');
+    } else if (stringField.includes('2')) {
+      resultArray.push('Boop!');
+    } else if (stringField.includes('1')) {
+      resultArray.push('Beep!');
+    } else {
+      resultArray.push(i);
+    }
+  }
+  return resultArray;
+}
+
+// UI logic
+window.onload = function() {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const input = document.getElementById('number').value;
+    const resultArray = getResultArray(input);
+    const output = document.getElementById('output');
+    output.innerHTML = resultArray.join(', ');
+  });
+};
