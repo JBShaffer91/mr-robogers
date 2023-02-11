bbe379c14177cba3a70542636aa3d100e353b1c5
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-10 16:14:53 -0800

    add random Mr Rogers gifs to the page

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,15 @@ function getResultArray(input) {
   return resultArray;
 }
 
+const mrRogersGifs = [
+  'https://i.pinimg.com/originals/22/67/75/226775d1db05b6e3e0a6d9310ba176c2.gif',
+  'https://thumbs.gfycat.com/ColossalWelloffBunting-size_restricted.gif',
+  'https://blog.lootcrate.com/wp-content/uploads/2020/04/giphy.gif',
+  'https://i.chzbgr.com/full/5395530752/h0DCD332B/looks-like-bob-ross-to-me',
+  'https://media.giphy.com/media/8OJdqYqN1Nii3UTD6l/giphy.gif',
+  'https://images.squarespace-cdn.com/content/v1/52652cf5e4b0469f698ba94d/1563475806449-4G9QSBQI35CMKRZNIGH6/bob03+%281%29.gif',
+];
+
 // UI logic
 window.onload = function() {
   const form = document.getElementById('form');
@@ -31,6 +40,11 @@ window.onload = function() {
     const output = document.getElementById('output');
     output.innerHTML = resultArray.join(', ');
   });
+
+  const gifContainer = document.getElementById('gif-container');
+  const randomGifIndex = Math.floor(Math.random() * mrRogersGifs.length);
+  const randomGifUrl = mrRogersGifs[randomGifIndex];
+  gifContainer.innerHTML = `<img src="${randomGifUrl}" alt="Mr. Rogers">`;
 
   const themeSwitch = document.querySelector('.theme-switch');
   function switchTheme(e) {
